dc003b921039a8eff26cfcff4b85bc529d72afbb
Vit0rg <vgois032@gmail.com>
2022-10-10 17:36:48 -0300

    Otimização do js e css

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 const botao = document.querySelector("#botao");
+const resultado = document.getElementById("result")
 
 botao.addEventListener('click', calcularMedia);
 
-function calcularMedia() {
+function calcularMedia() 
+{
   let valor = document.getElementById("valor").value;
   let peso = document.getElementById("peso").value;
   valor = valor.split(",");
   peso = peso.split(",");
-    part1 = [0];
-    for(let i = 0; i < valor.length; i++){
-    part1[i] = valor[i] * peso[i]
-    
-}
-    let part2 = 0;
-    for(let i = 0; i < part1.length; i++){
-    part2 = part2 + part1[i]
-    console.log(part2)
-}
-    let part3 = 0;
-    for(let i = 0; i < peso.length; i++){
-    part3 = parseFloat(peso[i]) + part3
-    }
-  final = part2 / part3;
-  finish = document.getElementById("result")
-  if (isNaN(final))
-    {
-      finish.innerHTML = ("Insira apenas números separados por vírgulas")
-      return;
-    }
-  finish.innerHTML = "A média ponderada dos valores: <br>" + valor + "<br> combinados com os pesos: <br>" + peso +
-    "<br> é igual a: <br>" + final
+  
+  if (valor.length !== peso.length ) 
+  {
+    resultado.innerHTML = ("Insira a mesma quantidade de argumentos nos 2 campos")
+    return;
+  }
+
+  let valor_ponderado = new Array;
+
+  for(let i = 0; i < valor.length; i++)
+  {
+    valor_ponderado[i] = parseFloat(valor[i] * peso[i]).toFixed('2')
+    console.log(valor_ponderado[i],valor[i], peso[i])
+  }
+
+  let media_ponderada = 0;
+  
+  for(let i = 0; i < valor_ponderado.length; i++)
+  {
+    media_ponderada += parseFloat(valor_ponderado[i])
+  }
+
+  if (isNaN(media_ponderada))
+  {
+    resultado.innerHTML = ("Insira apenas números separados por vírgulas")
+    return;
+  }
+  
+  resultado.innerHTML = "A média ponderada dos valores: <br>"
+                        + valor + "<br> combinados com os pesos: <br>"
+                        + peso + "<br> é igual a: <br>" 
+                        + parseFloat(media_ponderada).toFixed('2')
 }
